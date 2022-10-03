6366ef012992afbf1614c60748d3a7c3cbc8a4c8
Kristian Quirapas <kmquirapas@gmail.com>
2022-10-04 02:38:37 +0800

    almost done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from "next/app";
 import { WalletContextProvider } from "../contexts/ContextProvider";
+import { ThirdwebProvider } from '@thirdweb-dev/react/solana'; 
+import { Network } from '@thirdweb-dev/sdk/solana'; 
 import "../styles/globals.css";
+
+const network: Network = "devnet";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WalletContextProvider>
-      <Component {...pageProps} />
-    </WalletContextProvider>
+    <ThirdwebProvider endpoint={network}>
+      <WalletContextProvider>
+        <Component {...pageProps} />
+      </WalletContextProvider>
+    </ThirdwebProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { WalletMultiButton, } from "@solana/wallet-adapter-react-ui";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
+
+import { useWallet } from '@solana/wallet-adapter-react';
+import { useProgram, useProgramMetadata, useNFTs } from '@thirdweb-dev/react/solana';
 
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const Home: NextPage = () => {
   const router = useRouter();
+  const { data: program } = useProgram("b1tfCcFdcmxPGxKUZZjSdBf9EV4qwQcxy4DapTC1anf", "nft-collection");
+  const { data: metadata } = useProgramMetadata(program);
+  const { data: nfts } = useNFTs(program);
+
+  useEffect(() => {
+    // on mount
+  }, []);
 
   return (
     <>
@@ -16,23 +27,21 @@ const Home: NextPage = () => {
           <img src={"/thirdweb.svg"} className={styles.icon} />
           <img src={"/sol.png"} className={styles.icon} />
         </div>
-        <h1 className={styles.h1}>Solana, meet thirdweb 👋</h1>
-        <p className={styles.explain}>
-          Explore what you can do with thirdweb&rsquo;s brand new{" "}
-          <b>
-            <a
-              href="https://github.com/thirdweb-dev/js/tree/main/packages/solana"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.lightPurple}
-            >
-              Solana SDK
-            </a>
-          </b>
-          .
-        </p>
+        
+        <h1 className={styles.h1}>{metadata?.name}</h1>
+        <p className={styles.explain}>Would be nice to have collection description here</p>
 
         <WalletMultiButton />
+        
+        {!nfts ? (
+          <p>Loading...</p>
+        ) : (
+          <main className={styles.iconContainer}>
+            {nfts.map((nft, idx) => (
+              <img className={styles.thumbnail} key={idx} src={nft.image} />
+            ))}
+          </main>
+        )}
       </div>
     </>
   );
